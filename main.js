b94d0c4a3baf773b3b4359e30271140eeb368b0a
Sheikh <kenneth.sheikh@hotmail.com>
2024-01-11 12:43:19 +0100

    added functions to js file and some styling

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-function addThreeNumbers (num1, num2, num3) {
-return num1 + num2 + num3;
-}
+// function addThreeNumbers (num1, num2, num3) {
+// return num1 + num2 + num3;
+// }
 
-let resultOfSum = addThreeNumbers (10, 20, 50);
-console.log("result", result);
+// let resultOfSum = addThreeNumbers (10, 20, 50);
+// console.log("result", result);
+
+
+function multipltThreeNumbers (num1, num2, num3) {
+    return num1 * num2 * num3;
+}
+let resultOfSum = multipltThreeNumbers (2, 8, 30);
+console.log("result", resultOfSum);
